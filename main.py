44abf837a5a0e30a187eb2855b80a0b3594f8f77
Buggermenot <vermatanmay220@gmail.com>
2024-01-16 16:20:48 +0530

    Working Code. :D

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import openpyxl as xl
 from tkinter import filedialog
 import re
+from datetime import datetime as dt
+import datetime
+import random
 
 
 class TimeTable():
@@ -22,7 +25,6 @@ class TimeTable():
         self.rows = [[cell.value for cell in row] for row in self.rows]
         self.rows = [[cell.rstrip('\n').split('\n') if cell else [] for cell in row] for row in self.rows]
 
-    
     def removeOtherCourses(self):
         """Removes other courses you are not registerd to
         Ensure you have correctly added your courses to your_courses.txt
@@ -55,7 +57,8 @@ class TimeTable():
                 nrow.append(ncell)
             self.rows_updated.append(nrow)
 
-    def convertToCalendarEvents(self):
+    def getCourseDetails(self):
+        """Structures courses to have fields for .ics files."""
         timings = [
             '8:30-9:25',
             '9:30-10:25',
@@ -83,25 +86,96 @@ class TimeTable():
             
             class_ele = class_ele.replace(room, "")
             class_ele = class_ele.strip(': ')
+            room = room[1:-1]
 
             startt, endt = timings[row]
             date = dates[col]
 
-            return date, startt, endt, class_ele, room
+            startt = dt.strptime(f'{date} {startt}', '%d/%m/%Y %H:%M')
+            endt = dt.strptime(f'{date} {endt}', '%d/%m/%Y %H:%M')
+
+            for cc in self.clist:
+                if cc in class_ele:
+                    course_code = cc
+
+            return startt, endt, course_code, class_ele, room
 
 
-        self.calendar_events = []
+        self.course_dets = []
         si, sj = len(self.rows_updated), len(self.rows_updated[0])
         for i in range(si):
             for j in range(sj):
                 course = getCourse(i, j)
                 if course:
-                    self.calendar_events.append(course)
+                    self.course_dets.append(list(course))
+    
+    def rewriteCourseNames(self):
+        
+        print("Rewriting course codes as their course names...")
+        with open('course_codes.txt', 'r') as file:
+            cc = file.readlines()
+
+        course_codes = []
+        for c in cc:
+            if c[0] != '#' and c != '\n':
+                course_codes.append(c.rstrip('\n').split(': '))
+
+        course_codes = dict((cc, cname) for cc, cname in course_codes)
+
+        self.course_codes = course_codes
+
+
+        # Replace course code with course name
+        for i in range(len(self.course_dets)):
+            dets = self.course_dets[i]
+            cc = dets[2]
+            self.course_dets[i][3] = dets[3].replace(cc, course_codes[cc])
+
+    def makeICS(self):
+        ics_header = """BEGIN:VCALENDAR
+VERSION:2.0
+PRODID:- GitHub: BaronTime-Buggermenot, Time Table
+X-WR-CALNAME;VALUE=TEXT:Generated TimeTable Baron-Buggermenot [GitHub]
+
+BEGIN:VTIMEZONE
+TZID:Asia/Calcutta
+X-LIC-LOCATION:Asia/Calcutta
+BEGIN:STANDARD
+TZOFFSETFROM:+0530
+TZOFFSETTO:+0530
+TZNAME:IST
+DTSTART:19700101T000000
+END:STANDARD
+END:VTIMEZONE
+"""
+        
+        ics_events = "\n"
+
+        random.seed(170008200022)
+        for week in range(21):      # Weeks until first week of June
+            for startt, endt, course_code, class_desc, room in self.course_dets:
+                startt = startt + datetime.timedelta(weeks=week)
+                endt = endt + datetime.timedelta(weeks=week)
+                day = dt.strftime(startt, "%w")
+
+                ics_events += "BEGIN:VEVENT\n"
+                ics_events += f"SUMMARY:{class_desc}\n"
+                ics_events += f"DTSTAMP:20002208T000000\n"
+                ics_events += f"DESCRIPTION:Class Time!\n"
+                ics_events += f"DTSTART:{dt.strftime(startt, "%Y%m%dT%H%M%S")}\n"
+                ics_events += f"DTEND:{dt.strftime(endt, "%Y%m%dT%H%M%S")}\n"
+                ics_events += f"LOCATION:{room}\n"
+                ics_events += f"UID:{str(week).zfill(2)}{str(random.randint(0, 100)).zfill(2)}{day}{course_code}\n"
+                ics_events += "END:VEVENT\n\n"
+
+        with open("TimeTable Baron.ics", 'w') as timeTable:
+            timeTable.writelines(ics_header + ics_events + "END:VCALENDAR")
 
     def compile(self):
-        self.removeOtherCourses()
-        self.convertToCalendarEvents()
-
+        self.removeOtherCourses()       # Removes all other courses not in your_courses.txt
+        self.getCourseDetails()         # Structures and returns course details as: start_time, end_time, course_code, description, room
+        self.rewriteCourseNames()       # Replaces course_code with course name in decription
+        self.makeICS()                  # Generates .ics file for the entire semester.
                  
 
 
